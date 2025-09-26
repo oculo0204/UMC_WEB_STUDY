fcb8eff01200dfdcfb4136b704e9ed40e8c1a2f9
oculo0204 <96184457+oculo0204@users.noreply.github.com>
2025-09-26 14:56:46 +0900

    feat: ts ongoing

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -6,18 +6,22 @@ function App() {
     <>
       <div className='todo-wrap'>
         <header>TODO LIST</header>
-        <form className='input-wrap'>
+        <form id='todo-form'>
           <input id="todo-input" type="text" placeholder='할 일 입력'/>
           <button type="submit">할 일 추가</button>
         </form>
         <div className='show-wrap'>
           <section className='ongoing-wrap'>
             <h1>할 일</h1>
-            <div className='ongoing-content'><p>매튜</p><button>완료</button></div>
+            <ul id='todo-list'>
+              <li><p>매튜</p><button>완료</button></li>
+            </ul>
           </section>
           <section className='complete-wrap'>
             <h1>완료</h1>
-            <div className='complete-content'><p>오타니</p><button>삭제</button></div>  
+            <ul id='complete-list'>
+              <li><p>오타니</p><button>삭제</button></li>  
+            </ul>  
           </section>
         </div>
       </div>
